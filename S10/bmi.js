cb55b7fef66e2dc48eb1efd04527394346ba8135
aidamary <112872142+aidamary@users.noreply.github.com>
2022-09-26 21:42:00 +0100

    s7

---
+++ S10/bmi.js
@@ -0,0 +1,22 @@
+function calculateBMI(event) {
+    event.preventDefault()
+    const weight= document.getElementById("weight").value
+    const height= document.getElementById("height").value
+    if (!weight || !height) {
+        document.getElementById("bmi-result").innerHTML = "You need to fill all the fields!"
+        return;
+    }
+    const bmi= weight/((height/100)**2)
+    document.getElementById("bmi-result").innerHTML = "Your BMI is:" + bmi.toFixed(2)
+    let message;
+    if (bmi<18.5) {
+       message = "You are underweight"
+    } else if (bmi>=18.5 && bmi < 25) {
+        message = "You are a healthy weight"
+    } else if (bmi>=25 && bmi <30){
+        message = "You are overweight"
+    } else { 
+        message = "You are obese"
+    }
+    document.getElementById("bmi-message").innerHTML = message
+} 
