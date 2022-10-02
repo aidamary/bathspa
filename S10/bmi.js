091bb791c013d510df524721e3b98b7aa80834a6
aidamary <112872142+aidamary@users.noreply.github.com>
2022-10-02 14:23:51 +0200

    s12

--- S10/bmi.js
+++ S10/bmi.js
@@ -18,5 +18,19 @@ function calculateBMI(event) {
     } else { 
         message = "You are obese"
     }
+
+    // switch(true) {
+    //     case (bmi<18.5):
+    //         message = "You are underweight"
+    //         break;
+    //     case (bmi>=18.5 && bmi < 25):
+    //         message = "You are a healthy weight"
+    //         break;
+    //     case (bmi>=25 && bmi <30):
+    //         message = "You are overweight"
+    //         break
+    //     default:
+    //         message = "You are obese"
+    //  }
     document.getElementById("bmi-message").innerHTML = message
 } 
