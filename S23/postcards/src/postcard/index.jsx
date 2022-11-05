8d7159e11a3c51083143f63680c85c5cf47d3bd0
aidamary <imashate@gmail.com>
2022-11-05 14:41:59 +0000

    aida commit

--- S23/postcards/src/postcard/index.jsx
+++ S23/postcards/src/postcard/index.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Image from '../image'
 import './index.css';
 
+// This is a component by Aida
 const Postcard = ({url, children}) => {
   return (
     <div className='postcard-wrapper'>
